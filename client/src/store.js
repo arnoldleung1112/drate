c37d6bbce5389a3e6902769e5136bbaf9a1e2491
Arnold <arnoldleung1112@gmail.com>
2019-06-13 18:45:55 +0800

    update store js

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,22 @@
-
-import { createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootRecuder from './reducers';
+import rootReducer from './reducers';
 
-const middleware = [thunk];
+
 const initialState = {};
+const middleware = [thunk];
 
-const store = createStore( rootRecuder,
-                            initialState,
-                            compose(applyMiddleware(...middleware),
-                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                            )
-                        );
+// dev tools middleware
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeSetup(
+        applyMiddleware(...middleware)
+    )
+);
 
 export default store;
